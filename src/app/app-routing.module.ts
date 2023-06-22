ff61855db9aa8717a0e0b731b809bb0eb7242840
Gabriel Scarlat <gabrielscarlat36@gmail.com>
2023-06-22 14:23:48 +0200

    aggiunti path necessari commessa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,9 @@ import { RisorsaListComponent } from './features/risorsa/risorsa-list/risorsa-li
 import { AziendaListComponent } from './features/azienda/azienda-list/azienda-list.component';
 import { CommessaListComponent } from './features/commessa/commessa-list/commessa-list.component';
 import { CommessaDetailComponent } from './features/commessa/commessa-detail/commessa-detail.component';
+import { CommessaCreateComponent } from './features/commessa/commessa-create/commessa-create.component';
+import { CommessaEditComponent } from './features/commessa/commessa-edit/commessa-edit.component';
+import { CommessaDeleteComponent } from './features/commessa/commessa-delete/commessa-delete.component';
 
 const routes: Routes = [
   { path: 'welcome', component: WelcomeComponent },
@@ -12,6 +15,9 @@ const routes: Routes = [
   { path: 'azienda/list', component: AziendaListComponent },
   { path: 'commessa/list', component: CommessaListComponent },
   { path: 'commessa/detail/:id', component: CommessaDetailComponent },
+  { path: 'commessa/create', component: CommessaCreateComponent },
+  { path: 'commessa/edit/:id', component: CommessaEditComponent },
+  { path: 'commessa/delete/:id', component: CommessaDeleteComponent },
   { path: '', redirectTo: '/welcome', pathMatch: 'full' },
 ];
 
